b447191c40f1b5a24c8e7e3887eac65cf2347ecf
topvirtualbox <topvirtualbox@gmail.com>
2021-06-22 14:34:39 -0300

    Add operation buttons and equals button

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operation");
+const equals = document.querySelector("#equals");
 const clear = document.querySelector("#clear");
 const previous = document.querySelector("#previous");
 const current = document.querySelector("#current");
+
+let n1 = "";
+let n2 = "";
+let operator = "";
 
 numbers.forEach(button => {
     button.addEventListener("click", () => {
@@ -13,6 +19,53 @@ numbers.forEach(button => {
     });
 });
 
+operations.forEach(button => {
+    button.addEventListener("click", () => {
+        if (current.textContent === "" || current.textContent === ".") return;
+        if (n1 === "") {
+            n1 = Number(current.textContent);
+            if (previous.textContent.indexOf("=") === -1) {
+                previous.textContent += " " + current.textContent + " " + button.textContent;
+            } else {
+                previous.textContent = " " + current.textContent + " " + button.textContent;
+            }
+        } else if (n1 !== "") {
+            n2 = Number(current.textContent);
+            n1 = operate(n1, n2);
+            previous.textContent = " " + n1 + " " + button.textContent;
+            n2 = "";
+        }
+        operator = button.textContent;
+        current.textContent = "";
+    });
+});
+
+equals.addEventListener("click", () => {
+    if (n1 === "" || current.textContent === "" || current.textContent === ".") return;
+    n2 = Number(current.textContent);
+    previous.textContent += " " + current.textContent + " =";
+    current.textContent = operate(n1, n2);
+    n1 = "";
+    n2 = "";
+    operator = "";
+});
+
 clear.addEventListener("click", () => {
     current.textContent = "";
+    previous.textContent = "";
+    n1 = "";
+    n2 = "";
+    operator = "";
 });
+
+function operate(a, b) {
+    if (operator === "+") {
+        return a + b;
+    } else if (operator === "-") {
+        return a - b;
+    } else if (operator === "*") {
+        return a * b;
+    } else if (operator === "/") {
+        return a / b;
+    }
+}
