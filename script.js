072749ee84b336b3f6f18b6f556a0b238274b4c9
topvirtualbox <topvirtualbox@gmail.com>
2021-06-28 14:03:48 -0300

    Add a way to switch operator in the middle of an operation

--- script.js
+++ script.js
@@ -13,39 +13,36 @@ let operator = "";
 
 numbers.forEach(button => {
     button.addEventListener("click", () => {
-        if (button.textContent === "." && current.textContent.indexOf(".") === -1) {
-            current.textContent += button.textContent;
-        } else if (button.textContent !== ".") {
-            current.textContent += button.textContent;
-        }
+        if (button.textContent === "." && current.textContent.includes(".")) return;
+        current.textContent += button.textContent;
     });
 });
 
 operations.forEach(button => {
     button.addEventListener("click", () => {
-        if (current.textContent === "" || current.textContent === "." || current.textContent === "-") return;
+        if (current.textContent === "" && n1 === "" || current.textContent === "." || current.textContent === "-" || current.textContent === "-.") return;
+        if (current.textContent === "") {
+            operator = button.textContent;
+            previous.textContent = previous.textContent.slice(0, -1) + operator;
+            return;
+        }
         if (n1 === "") {
             n1 = Number(current.textContent);
-            if (previous.textContent.indexOf("=") === -1) {
-                previous.textContent += " " + current.textContent + " " + button.textContent;
-            } else {
-                previous.textContent = " " + current.textContent + " " + button.textContent;
-            }
-        } else if (n1 !== "") {
+        } else {
             n2 = Number(current.textContent);
             n1 = operate(n1, n2);
-            previous.textContent = " " + n1 + " " + button.textContent;
             n2 = "";
         }
         operator = button.textContent;
+        previous.textContent = n1 + " " + operator;
         current.textContent = "";
     });
 });
 
 equals.addEventListener("click", () => {
-    if (n1 === "" || current.textContent === "" || current.textContent === "." || current.textContent === "-") return;
+    if (n1 === "" || current.textContent === "" || current.textContent === "." || current.textContent === "-" || current.textContent === "-.") return;
     n2 = Number(current.textContent);
-    previous.textContent += " " + current.textContent + " =";
+    previous.textContent += " " + n2 + " =";
     current.textContent = operate(n1, n2);
     n1 = "";
     n2 = "";
@@ -53,8 +50,8 @@ equals.addEventListener("click", () => {
 });
 
 clear.addEventListener("click", () => {
-    current.textContent = "";
     previous.textContent = "";
+    current.textContent = "";
     n1 = "";
     n2 = "";
     operator = "";
@@ -65,23 +62,17 @@ backspace.addEventListener("click", () => {
 });
 
 negative.addEventListener("click", () => {
-    if (current.textContent.indexOf("-") === -1) {
-        current.textContent = "-" + current.textContent;
-    } else {
+    if (current.textContent.includes("-")) {
         current.textContent = current.textContent.slice(1);
+    } else {
+        current.textContent = "-" + current.textContent;
     }
 });
 
 function operate(a, b) {
-    if (operator === "+") {
-        return a + b;
-    } else if (operator === "-") {
-        return a - b;
-    } else if (operator === "*") {
-        return a * b;
-    } else if (operator === "/") {
-        return a / b;
-    } else if (operator === "%") {
-        return b / 100 * a;
-    }
+    if (operator === "+") return a + b;
+    else if (operator === "-") return a - b;
+    else if (operator === "*") return a * b;
+    else if (operator === "/") return a / b;
+    else if (operator === "%") return b / 100 * a;
 }
